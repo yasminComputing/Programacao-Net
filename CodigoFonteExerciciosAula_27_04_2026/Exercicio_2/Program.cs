ee166824ea2ba10e68e2ee0fa3061eb2d9e6e0c3
Yasmin <yasmindeitos1959@gmail.com>
2026-05-02 20:27:09 -0300

    adicionando exercicios

---
+++ CodigoFonteExerciciosAula_27_04_2026/Exercicio_2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe um número: ");
+                decimal numero1 = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Informe outro número para divisão: ");
+                decimal numero2 = decimal.Parse(Console.ReadLine());
+
+                decimal divisao = numero1 / numero2;
+
+                Console.WriteLine($"Resultado da divisão: {divisao}");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Não existe divisão por zero na matemática real");
+            }
+        }
+    }
+}
---
+++ CodigoFonteExerciciosAula_27_04_2026/Exercicio_8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Informe um valor para saque: R$ ");
+                decimal valor = decimal.Parse(Console.ReadLine());
+                ContaBancaria conta = new ContaBancaria();
+                conta.Sacar(valor);
+            }catch (SaldoInsuficienteException e)
+            {
+                Console.WriteLine($"ERRO:{e.Message}");
+            }
+        }
+    }
+}
